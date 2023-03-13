fb560944493dd989112847ac728f3d014da0f471
vedran97 <vedantmranade@gmail.com>
2023-03-12 21:46:30 -0400

    Added obstacle using semi algebra
    Added ability to take input
    Added time calculation for djikstra and viz
    updated readme
    Inverted Y axis to relocate the origin to bottom left

--- .ipynb_checkpoints/dijkstra_Vedant_Ranade-checkpoint.py
+++ .ipynb_checkpoints/dijkstra_Vedant_Ranade-checkpoint.py
@@ -1,4 +1,5 @@
 #Imports
+#Libraries
 import numpy as np
 import cv2
 from tqdm import tqdm
@@ -11,7 +12,7 @@ import time
 
 ## Constants
 # y direction is a row # x direction is a column. Operations are y,x or row,column
-
+print("\r\nGENERATING OBSTACLE MAP")
 YBOUND = range(5,245,1) # Padding of 5mm on each dimension
 XBOUND = range(5,595,1) # Padding of 5mm on each dimension
 ACTIONS = {"U":(+1,0),"D":(-1,0),"L":(0,-1),"R":(0,+1),"UL":(+1,-1),"UR":(+1,+1),"DR":(-1,+1),"DL":(-1,-1)}
@@ -20,26 +21,62 @@ SIDEWAY_COST = 1.0
 COSTFORACTION = {"U":SIDEWAY_COST,"D":SIDEWAY_COST,"L":SIDEWAY_COST,"R":SIDEWAY_COST,"UL":DIAGONAL_COST,"UR":DIAGONAL_COST,"DR":DIAGONAL_COST,"DL":DIAGONAL_COST}
 OBSTACLE_COLOR = [255,0,0]
 obstacle_image = np.full((YBOUND[-1]+5+1,XBOUND[-1]+5+1,3),125,dtype=np.uint8)
+# Define obstacles
 
-#USES CV2 polyfill
-def makeObstacleMap(image):
-    #Rectangle 1:
-    # 5 here clearly indicates the 5mm clearance.
-    image = cv2.rectangle(image, (99-5,0) , (149+5,99+5), OBSTACLE_COLOR ,  -1)
-    #Rectangle 2:
-    image = cv2.rectangle(image, (99-5,149-5) , (149+5,249), OBSTACLE_COLOR ,  -1)
-    #Triangle 1:
-    triangle_corners = [(460-5-1, int(25.0-1.0-(5.0*np.sqrt(2)))), (460-5-1, int(225.0-1.0+(5.0*np.sqrt(2)))), (int(510.0+(5.0*np.sqrt(2))-1), 125-1)]
-    image = cv2.fillPoly(image, np.array([triangle_corners]), OBSTACLE_COLOR)
-    #Hexagon 1:
-    hex_corners = [(235-1, 163-1),(300-1,200-1),(365-1,163-1),(365-1,88-1),(300-1,50-1),(235-1,88-1)]
-    image = cv2.fillPoly(image, np.array([hex_corners]), OBSTACLE_COLOR)
+# Rectangles
+def rectangle1(p):
+    if p[0] > (100-5) and p[0]<(150+5) and p[1]<(100+5):
+        return False
+    else:
+        return True
+    
+def rectangle2(p):
+    if p[0] > (100-5) and p[0]<(150+5) and p[1]>(150-5):
+        return False
+    else:
+        return True  
+
+# Hexagon
+P_act = [[235,162.5],[300,200],[365,162.5],[365,87.5],[300,50],[235,87.5]]
+P     = [[230.7,165],[300,205],[369.3,165],[369.3,85],[300,45],[230.7,85]]
+L1 = np.polyfit([P[0][0],P[1][0]] , [P[0][1],P[1][1]] , 1)
+L2 = np.polyfit([P[1][0],P[2][0]] , [P[1][1],P[2][1]] , 1)
+L3 = np.polyfit([P[3][0],P[4][0]] , [P[3][1],P[4][1]] , 1)
+L4 = np.polyfit([P[4][0],P[5][0]] , [P[4][1],P[5][1]] , 1)
+
+def hexagon(p):
+    L1_ = p[1] - L1[0]*p[0] - L1[1] <0
+    L2_ = p[1] - L2[0]*p[0] - L2[1] <0
+    L3_ = p[1] - L3[0]*p[0] - L3[1] >0
+    L4_ = p[1] - L4[0]*p[0] - L4[1] >0
+    if L1_ and L2_ and L3_ and L4_ and p[0]>230.7 and p[0]<369.3:
+        return False
+    else:
+        return True
+    
+# Triangle
+Q_act = [[460,225],[510,125],[460,25]]
+Q     = [[455,238],[517,125],[455,10]]
+Q1 = np.polyfit([Q[0][0],Q[1][0]] , [Q[0][1],Q[1][1]] , 1)
+Q2 = np.polyfit([Q[1][0],Q[2][0]] , [Q[1][1],Q[2][1]] , 1)
+
+def triangle(p):
+    Q1_ = p[1] - Q1[0]*p[0] - Q1[1] <0
+    Q2_ = p[1] - Q2[0]*p[0] - Q2[1] >0
+    if Q1_ and Q2_ and p[0]>455:
+        return False
+    else:
+        return True
+    
+def npObstacleMap(image):
+    for y in range(image.shape[0]):
+        for x in range(image.shape[1]):
+            if (not triangle((x,y))) or (not rectangle1((x,y))) or (not rectangle2((x,y))) or (not hexagon((x,y))):
+                image[y,x] = OBSTACLE_COLOR
     return image
 
-#USES NUMPY POLYFIT WITH VERTICES:
-#DEFINE VERTICES AND OBSTACLES:
-print("\r\nGENERATING OBSTACLE MAP")
-OBSTACLE_MAP = makeObstacleMap(obstacle_image)
+OBSTACLE_MAP = npObstacleMap(obstacle_image)
+# cv2.imwrite('./OBSTACLE_MAP.png',OBSTACLE_MAP)
 print("\r\nFINISHED GENERATING OBSTACLE MAP")
 #Node data structure
 class GraphNode:
@@ -240,6 +277,7 @@ def vizExplore(visitedNodesPerFrame,path):
         frame[y,x] = [0,255,0]
     return empty_images
 
+## Runs everything, saves a video
 def djikstraViz(start,end,input_num=0):
     if start[0] not in YBOUND or start[1] not in XBOUND:
         print("START point outside of bounds")
@@ -252,19 +290,17 @@ def djikstraViz(start,end,input_num=0):
         return
     before  = time.time()
     path,visitedNodes = dikPrintReversePath(start,end,False)
-    print("TIME IN SECONDS FOR DJIKSTRA SOLN:{}".format(time.time()-before))
+    print("TIME FOR DJIKSTRA SOLN:{}".format(time.time()-before))
     if path is None or visitedNodes is None:
         print("\r\n NO OUTPUT GENERATED \r\n")
         return 
-    
-    before = time.time()
+    print("\r\n STARTED VISUALIZATION \r\n")
+    before  = time.time()
     visitedNodesPerFrame = findVisitedNotesPerFrame(path,visitedNodes)
-    
     assert len(visitedNodesPerFrame) == len(path)
     viz = vizExplore(visitedNodesPerFrame,path)
     pathViz = vizPath(viz,path)
-    
-    print("\r\n GENERATING OUTPUT VIDEO \r\n")
+
     size = (pathViz[0].shape[1],pathViz[0].shape[0])
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     voObj = cv2.VideoWriter('./viz/PathViz'+str(input_num)+'.mp4',fourcc, 15,size)
@@ -273,7 +309,7 @@ def djikstraViz(start,end,input_num=0):
         image = frame
         voObj.write(image)
     voObj.release()
-    print("TIME IN SECONDS FOR VIZUALIZATION:{}".format(time.time()-before))
+    print("TIME FOR VISUALIZATION OUTPUT SOLN:{}".format(time.time()-before))
     print("\r\nFINISHED GENERATING OUTPUT VIDEO at ./viz/ \r\n")
     return
 
@@ -283,5 +319,4 @@ while True:
     endY = int(input('End Point Y(Row) coordinate'))
     endX = int(input('End Point X(Column) coordinate'))
     break
-    
 djikstraViz((startY,startX),(endY,endX))
